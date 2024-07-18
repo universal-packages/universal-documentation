0873521766311cedb5c39d47ef16b3cc06c474cd
David De Anda <omarandstuff@gmail.com>
2024-07-18 17:15:44 -0600

    Keep up with core refinements

--- tutorials/todo-list/tests/ExampleApp.test.js
+++ tutorials/todo-list/tests/ExampleApp.test.js
@@ -1,9 +1,9 @@
-import { runApp } from '@universal-packages/core'
+import { runApp } from "@universal-packages/core";
 
-describe('ExpressApp', () => {
-  it('works', async () => {
-    const stopApp = await runApp('example-app')
+describe("ExpressApp", () => {
+  it("works", async () => {
+    const stopApp = await runApp("example-app", { exitType: "throw" });
 
-    await stopApp()
-  })
-})
+    await stopApp();
+  });
+});
